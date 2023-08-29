a71d20ad3338de1e4692d0142a48b34c840d5f99
Manue-droi <manusarf16@gmail.com>
2023-08-28 20:33:31 -0500

    cdcd

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -29,22 +29,22 @@ public class TV {
 		}
 	}	
 	public void canalUp() {
-		if(canal >= 1 && canal < 120 && estado == true) {
+		if(canal < 120 && estado == true) {
 			this.canal ++;	
 		}
 	}
 	public void canalDown() {
-		if(canal > 1 && canal <= 120 && estado==true) {
+		if(canal > 1 && estado== true ) {
 			this.canal --;
 		}
 	}
 	public void volumenUp() {
-		if(volumen >= 0 && volumen < 7 && estado == true) {
+		if( volumen < 7 && estado == true) {
 			this.volumen ++;
 		}
 	}
 	public void volumenDown() {
-		if(volumen > 0 && volumen <= 7 && estado==true) {
+		if(volumen > 0 && estado==true) {
 			this.volumen --;
 		}
 	}
